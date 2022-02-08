50b8544faec3d205f32060bf4caf3fcebbc315eb
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2022-02-09 01:29:56 +0530

    Remove database instantiation and builder from MilkDeliveryDatabase.kt

--- app/src/main/java/com/example/milkrecordkeeping/MilkRecordKeepingApplication.kt
+++ app/src/main/java/com/example/milkrecordkeeping/MilkRecordKeepingApplication.kt
@@ -1,15 +1,10 @@
 package com.example.milkrecordkeeping
 
-import com.example.milkrecordkeeping.database.MilkDeliveryDatabase
 import com.example.milkrecordkeeping.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class MilkRecordKeepingApplication : DaggerApplication() {
-    val database: MilkDeliveryDatabase by lazy {
-        MilkDeliveryDatabase.getDatabase(this)
-    }
-
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(applicationContext)
     }
--- app/src/main/java/com/example/milkrecordkeeping/database/MilkDeliveryDatabase.kt
+++ app/src/main/java/com/example/milkrecordkeeping/database/MilkDeliveryDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.milkrecordkeeping.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.milkrecordkeeping.data.*
+import com.example.milkrecordkeeping.data.Agent
+import com.example.milkrecordkeeping.data.AgentDao
+import com.example.milkrecordkeeping.data.EntriesDao
+import com.example.milkrecordkeeping.data.MilkEntries
 
 @Database(entities = [Agent::class, MilkEntries::class], version = 2, exportSchema = false)
 abstract class MilkDeliveryDatabase : RoomDatabase() {
@@ -12,24 +13,4 @@ abstract class MilkDeliveryDatabase : RoomDatabase() {
     abstract fun agentDao(): AgentDao
 
     abstract fun milkEntriesDao(): EntriesDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: MilkDeliveryDatabase? = null
-
-        fun getDatabase(context: Context): MilkDeliveryDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context,
-                    MilkDeliveryDatabase::class.java,
-                    "milk_delivery_database"
-                )
-                    .addMigrations(MIGRATION_1_2)
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
